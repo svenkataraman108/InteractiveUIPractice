89d574039bd907cfeb42fd0e1985bb5360dc5643
@svenkataraman108 <kpvenkat108@gmail.com>
2024-05-07 15:50:31 -0500

    Initial Commit

---
+++ InteractiveUIPractice/InteractiveUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIPracticeApp.swift
+//  InteractiveUIPractice
+//
+//  Created by Scholar on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
